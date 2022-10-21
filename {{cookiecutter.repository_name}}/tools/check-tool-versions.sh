a65f5315fccdd5611afe59639da44cd9baea29aa
Lee Myring <29373851+thinkstack@users.noreply.github.com>
2022-10-21 08:43:41 +0100

    TEL-3093: fix check tool versions script
    Co-authored-by: Stephen Palfreyman <18111914+sjpalf@users.noreply.github.com>

--- {{cookiecutter.repository_name}}/tools/check-tool-versions.sh
+++ {{cookiecutter.repository_name}}/tools/check-tool-versions.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
-# This script is to make sure both .tool-versions (ASDF) and .<tool>-version (individual version managers) are pointing
-#  to the same version of the given tool, to avoid divergence.
+# Ensure .tool-versions (ASDF) and .<tool>-version (individual version managers) are referencing same version
 
 # Variables
 root_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 asdf_version_file="$root_dir/.tool-versions"
-declare -a tools=("python" "terraform" "terragrunt")
+declare -a tools=("poetry" "python" "terraform" "terragrunt")
 all_synced=true
 
 # Checks
 for tool in "${tools[@]}"
 do
   tool_version_file="$root_dir/.$tool-version"
-  if diff <(cat "$asdf_version_file" | grep "$tool " | awk '{print $2}') <(cat "$tool_version_file") > /dev/null ; then
-    echo "$tool version files are in sync"
-  else
-    echo -e "$tool version divergence detected!\t Cross-check $(basename ${asdf_version_file}) and $(basename ${tool_version_file})"
-    all_synced=false
+  if [[ -f "${tool_version_file}" ]]; then
+    if diff <(grep "$tool " "${asdf_version_file}" | awk '{print $2}') <(cat "${tool_version_file}") > /dev/null ; then
+      echo "$tool version files are in sync"
+    else
+      echo -e "$tool version divergence detected!\t Cross-check $(basename "${asdf_version_file}") and $(basename "${tool_version_file}")"
+      all_synced=false
+    fi
   fi
 done
 
