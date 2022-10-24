897dba428ce2cbeb46331c17aa1eb626e1d11fef
Think Stack Limited <29373851+thinkstack@users.noreply.github.com>
2022-10-24 12:41:40 +0100

    TEL-3093: run tests using style parameter (#19)
    Co-authored-by: Rizina Khatun <66684341+rizinaa99@users.noreply.github.com>

--- {{cookiecutter.lambda_name_formatted}}/bin/run-tests.sh
+++ {{cookiecutter.lambda_name_formatted}}/bin/run-tests.sh
@@ -4,6 +4,14 @@ set -eu
 
 export LOG_LEVEL="DEBUG"
 export PYTHONPATH='src'
-coverage run --module {{ cookiecutter.unit_test_module }} tests/unit/*_test.py
+{% if cookiecutter.unit_test_style is defined -%}
+  {% if cookiecutter.unit_test_style == 'pytest' -%}
+pytest tests/unit --cov=src -vv
+  {% elif cookiecutter.unit_test_style == 'coverage' -%}
+coverage run --module unittest tests/unit/*_test.py
 coverage report --show-missing --skip-empty
+  {% else %}
+echo "No test module run, please specify 'pytest' or 'coverage' in cookiecutter.unit_test_style"
+  {%- endif %}
+{%- endif %}
 flake8 src
