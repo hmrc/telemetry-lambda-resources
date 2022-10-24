e62b6aa4c1b2967cb1c301ddb8f454cb303b4e4c
Lee Myring <29373851+thinkstack@users.noreply.github.com>
2022-10-24 13:02:03 +0100

    TEL-3093: reduce specificity of file matching
    Co-authored-by: Rizina Khatun <66684341+rizinaa99@users.noreply.github.com>

--- {{cookiecutter.lambda_name_formatted}}/bin/run-tests.sh
+++ {{cookiecutter.lambda_name_formatted}}/bin/run-tests.sh
@@ -8,7 +8,7 @@ export PYTHONPATH='src'
   {%- if cookiecutter.unit_test_style == 'pytest' -%}
 pytest tests/unit --cov=src -vv
   {%- elif cookiecutter.unit_test_style == 'coverage' -%}
-coverage run --module unittest tests/unit/*_test.py
+coverage run --module unittest tests/unit/*test.py
 coverage report --show-missing --skip-empty
   {%- else -%}
 echo "No test module run, please specify 'pytest' or 'coverage' in cookiecutter.unit_test_style"
