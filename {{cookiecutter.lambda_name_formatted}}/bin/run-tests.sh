7bf559281400e140ff8a5e0e316da6f4e64f58f7
Think Stack Limited <29373851+thinkstack@users.noreply.github.com>
2022-10-24 13:03:15 +0100

    TEL 3093 fix test script (#20)
    * TEL-3093: run tests using style parameter
    * TEL-3093: fix jinja whitespace issues
    * TEL-3093: reduce specificity of file matching
    
    Co-authored-by: Rizina Khatun <66684341+rizinaa99@users.noreply.github.com>

--- {{cookiecutter.lambda_name_formatted}}/bin/run-tests.sh
+++ {{cookiecutter.lambda_name_formatted}}/bin/run-tests.sh
@@ -4,14 +4,14 @@ set -eu
 
 export LOG_LEVEL="DEBUG"
 export PYTHONPATH='src'
-{% if cookiecutter.unit_test_style is defined -%}
-  {% if cookiecutter.unit_test_style == 'pytest' -%}
+{%- if cookiecutter.unit_test_style is defined -%}
+  {%- if cookiecutter.unit_test_style == 'pytest' -%}
 pytest tests/unit --cov=src -vv
-  {% elif cookiecutter.unit_test_style == 'coverage' -%}
-coverage run --module unittest tests/unit/*_test.py
+  {%- elif cookiecutter.unit_test_style == 'coverage' -%}
+coverage run --module unittest tests/unit/*test.py
 coverage report --show-missing --skip-empty
-  {% else %}
+  {%- else -%}
 echo "No test module run, please specify 'pytest' or 'coverage' in cookiecutter.unit_test_style"
-  {%- endif %}
-{%- endif %}
+  {%- endif -%}
+{%- endif -%}
 flake8 src
