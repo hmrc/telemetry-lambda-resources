f54ccd9cdf05e59cbb137e5ca4698d8339fedaf3
Lee Myring <29373851+thinkstack@users.noreply.github.com>
2022-10-24 12:09:51 +0100

    TEL-3093: run tests consistently using coverage
    Co-authored-by: Rizina Khatun <66684341+rizinaa99@users.noreply.github.com>

--- {{cookiecutter.lambda_name_formatted}}/bin/run-tests.sh
+++ {{cookiecutter.lambda_name_formatted}}/bin/run-tests.sh
@@ -4,5 +4,6 @@ set -eu
 
 export LOG_LEVEL="DEBUG"
 export PYTHONPATH='src'
-pytest tests/unit --cov=src -vv
-flake8 src/handler.py
+coverage run --module {{ cookiecutter.unit_test_module }} tests/unit/*_test.py
+coverage report --show-missing --skip-empty
+flake8 src
